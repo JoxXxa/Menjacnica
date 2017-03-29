ec89b11ee3b3b15f7e2041483b18a24a1afdb5bd
JoxXxa <jovpet96@gmail.com>
2017-03-29 21:58:16 +0200

    Redefinisane metode equals, hashCode i toString

--- Menjacnica/src/valute/Evro.java
+++ Menjacnica/src/valute/Evro.java
@@ -56,4 +56,62 @@ public class Evro {
 		this.srednjikurs = srednjikurs;
 	}
 
+	@Override
+	public String toString() {
+		return "Evro [ime=" + ime + ", skracenica=" + skracenica + ", datum=" + datum + ", prodajnikurs=" + prodajnikurs
+				+ ", kupovnikurs=" + kupovnikurs + ", srednjikurs=" + srednjikurs + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((datum == null) ? 0 : datum.hashCode());
+		result = prime * result + ((ime == null) ? 0 : ime.hashCode());
+		long temp;
+		temp = Double.doubleToLongBits(kupovnikurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		temp = Double.doubleToLongBits(prodajnikurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + ((skracenica == null) ? 0 : skracenica.hashCode());
+		temp = Double.doubleToLongBits(srednjikurs);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Evro other = (Evro) obj;
+		if (datum == null) {
+			if (other.datum != null)
+				return false;
+		} else if (!datum.equals(other.datum))
+			return false;
+		if (ime == null) {
+			if (other.ime != null)
+				return false;
+		} else if (!ime.equals(other.ime))
+			return false;
+		if (Double.doubleToLongBits(kupovnikurs) != Double.doubleToLongBits(other.kupovnikurs))
+			return false;
+		if (Double.doubleToLongBits(prodajnikurs) != Double.doubleToLongBits(other.prodajnikurs))
+			return false;
+		if (skracenica == null) {
+			if (other.skracenica != null)
+				return false;
+		} else if (!skracenica.equals(other.skracenica))
+			return false;
+		if (Double.doubleToLongBits(srednjikurs) != Double.doubleToLongBits(other.srednjikurs))
+			return false;
+		return true;
+	}
+	
+	
+
 }
