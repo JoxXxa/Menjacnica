980c218dcc6c6fbef447421f1cbf6508774f12d9
JoxXxa <jovpet96@gmail.com>
2017-03-30 00:25:38 +0200

    Dodata klasa koja implementira interfejs, trenutno ne funkcionalna

---
+++ Menjacnica/src/valute/Menjac.java
@@ -0,0 +1,27 @@
+package valute;
+
+import java.util.GregorianCalendar;
+
+import razmena.MenjacnicaInterfejs;
+
+public class Menjac implements MenjacnicaInterfejs {
+
+	@Override
+	public void dodavanjeKursa(GregorianCalendar datum, Kurs kurs) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void brisanjeKursa(GregorianCalendar datum) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Kurs pretragaKursa(GregorianCalendar datum, Kurs k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
